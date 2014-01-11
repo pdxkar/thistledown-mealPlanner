de73f9c58def0d437fc739a61c69e8cfaa732c69
pdxkar <kroot@catalystitservices.com>
2014-01-11 07:18:42 -0800

    added Beaverton house to DB friendly places in spring-servlet; routing fixed in app.js so we can get to about page

--- src/main/webapp/assets/js/app.js
+++ src/main/webapp/assets/js/app.js
@@ -7,34 +7,23 @@ myApp.config([
 		'$routeProvider',
 		'$locationProvider',
 		function($routeProvider, $locationProvider) {
-			$routeProvider.when(
-					'/component',
-					{
+			//When the url is "/mealPlanner/" then redirect to the browseComponents page
+			$routeProvider.when(__appContext__, {
+				templateUrl : __appContext__ + '/assets/partials/browseComponents.html',
+				controller : 'ComponentListCtrl'
+					//When the url is "/mealPlanner/component" then redirect to the component page
+					}).when('/component',{
 						templateUrl : __appContext__
 								+ '/assets/partials/browseComponents.html',
 						controller : 'ComponentListCtrl'
-
-					// This is to redirect to the new components html page
-					// i.e. when the url = localhost:8080/books/components, it
-					// goes the components.html page
+					//When the url is "/mealPlanner/about" then redirect to the about page		
+					}).when(__appContext__ + '/about', {
+						templateUrl : __appContext__ + '/assets/partials/aboutUs.html'
+					//if the url is "mealPlanner/*" then redirect to the "/browseComponents.html" page	
 					}).otherwise({
 				redirectTo : '/component'
 			});
 			$locationProvider.html5Mode(true);
 		} ]);
 
-/*
- * .when(__appContext__ + '/components', { templateUrl : __appContext__ +
- * '/assets/partials/browseComponents.html', controller : 'ComponentListCtrl'
- * 
- * }).when(__appContext__, { templateUrl : __appContext__ +
- * '/assets/partials/browseBooks.html', controller : 'BookListCtrl'
- * 
- * }).when(__appContext__ + '/about', { templateUrl : __appContext__ +
- * '/assets/partials/aboutUs.html'
- * 
- * }).when(__appContext__ + '/:isbn13', { templateUrl : __appContext__ +
- * '/assets/partials/books-detail.html', controller : 'BookDetailCtrlISBN'
- *  })
- */
 

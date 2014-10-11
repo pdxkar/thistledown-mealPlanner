a5a6b12c6299582fbe6545a4dd3386ce4c4a6e42
pdxkar <kroot@catalystitservices.com>
2014-10-11 08:18:27 -0700

    can fetch a record by id from db

--- src/main/java/com/books/controller/BooksController.java
+++ src/main/java/com/books/controller/BooksController.java
@@ -20,126 +20,12 @@ import com.books.booklister.Bookinator;
 import com.books.dao.ComponentDao;
 import com.books.model.Component;
 
+
+
 @Controller
 @RequestMapping(value = "/data")
 @Order(value = Ordered.HIGHEST_PRECEDENCE)
 public class BooksController {
-//	/**
-//	 * Inject (via configuration in spring-servlet.xml) an AuthorDao instance
-//	 */
-//	@Resource
-//	private AuthorDao authorDao;
-//
-//	public void setAuthorDao(AuthorDao authorDao) {
-//		this.authorDao = authorDao;
-//	}
-//
-//	public AuthorDao getAuthorDao() {
-//		return authorDao;
-//	}
-//
-//	/**
-//	 * Inject (via configuration in spring-servlet.xml) an BookDao instance
-//	 */
-//	@Resource
-//	private BookDao bookDao;
-//
-//	public void setBookDao(BookDao bookDao) {
-//		this.bookDao = bookDao;
-//	}
-//
-//	public BookDao getBookDao() {
-//		return bookDao;
-//	}
-//
-//	/**
-//	 * Inject (via configuration in spring-servlet.xml) an MediaDao instance
-//	 */
-//	@Resource
-//	private MediaDao mediaDao;
-//
-//	public void setMediaDao(MediaDao mediaDao) {
-//		this.mediaDao = mediaDao;
-//	}
-//
-//	public MediaDao getMediaDao() {
-//		return mediaDao;
-//	}
-//
-//	/**
-//	 * Inject (via configuration in spring-servlet.xml) an PhysicalBookDao
-//	 * instance
-//	 */
-//	@Resource
-//	private PhysicalBookDao physicalBookDao;
-//
-//	public void setPhysicalBookDao(PhysicalBookDao physicalBookDao) {
-//		this.physicalBookDao = physicalBookDao;
-//	}
-//
-//	public PhysicalBookDao getPhysicalBookDao() {
-//		return physicalBookDao;
-//	}
-//
-//	/**
-//	 * Inject (via configuration in spring-servlet.xml) an TagDao instance
-//	 */
-//	@Resource
-//	private TagDao tagDao;
-//
-//	public void setTagDao(TagDao tagDao) {
-//		this.tagDao = tagDao;
-//	}
-//
-//	public TagDao getTagDao() {
-//		return tagDao;
-//	}
-//	
-//	/**
-//	 * @param request
-//	 *            : value - URL relative to /Books; method - GET; headers -
-//	 *            Accept=/text/xml,application/json
-//	 * @return JSON string representing books (including authors, media, tags,
-//	 *         physical book)
-//	 * 
-//	 *         This endpoint returns the list of books in the database, as a
-//	 *         JSON object/string.
-//	 */
-//	@RequestMapping(method = RequestMethod.GET, headers = { "Accept=application/json" })
-//	public @ResponseBody
-//	List<Book> getListOfBooks() {
-//		System.out.println("BooksController GET /data");  //TODO Replace print statements with log4j logging
-//
-//		return Bookinator.getListOfBooks(bookDao, tagDao, authorDao, physicalBookDao, mediaDao);
-//	}
-//
-//	/**
-//	 * @param request
-//	 *            : value - URL relative to /Books/data; method - GET; headers -
-//	 *            Accept=/text/xml,application/json
-//	 * @return JSON string representing books (including authors, media, tags,
-//	 *         physical book)
-//	 * 
-//	 *         This endpoint returns a single book as a JSON object/string.
-//	 */
-//	@RequestMapping(value = "/{isbn}", method = RequestMethod.GET, headers = { "Accept=application/json" })
-//	public @ResponseBody
-//	Book getBookByIsbn(@PathVariable String isbn) {
-//		System.out.println("BooksController GET /data/{isbn}");  //TODO Replace print statements with log4j logging
-//
-//		return Bookinator.getBookByIsbn(bookDao, tagDao, authorDao, physicalBookDao, mediaDao, isbn);
-//	}
-//
-//	/**
-//	 * @return list of tags with count of books using each tag
-//	 */
-//	@RequestMapping(value = "/tags", method = RequestMethod.GET)
-//	public @ResponseBody
-//	List<TagCount> getListOfTags() {
-//		System.out.println("BooksController GET data/tags");  //TODO Replace print statements with log4j logging
-//
-//		return Bookinator.getListOfTagCounts(tagDao);
-//	}
 
 	/**
 	 * Handle any exceptions from queryForObject (e.g. query for bookid - not
@@ -169,7 +55,10 @@ public class BooksController {
 		return componentDao;
 	}
 	
-	//call this service with URL http://localhost:8080/mealPlanner/data/components?componentid=1	
+//	//this is the good one
+//	//call this service with URL http://localhost:8080/mealPlanner/data/components <= returns the list 	
+//	//call this service with URL http://localhost:8080/mealPlanner/data/  <= returns the js page
+//	//call this service with URL http://localhost:8080/mealPlanner/data/components?componentid=1 <= returns the whole list
 	@RequestMapping(value = "/components", method = RequestMethod.GET, headers = { "Accept=application/json" })
 	public @ResponseBody
 	List<Component> getListOfComponents() {
@@ -177,5 +66,19 @@ public class BooksController {
 
 		return Bookinator.getListOfComponents(componentDao);
 	}
+	
+	//TEST	
+//	@RequestMapping(value = "/components", method = RequestMethod.GET, headers = { "Accept=application/json" })
+//	public @ResponseBody
+//	Component getListOfComponents() {
+//		System.out.println("BooksController GET /data/components");  
+//		Component component = new Component();
+//		component.setItemid(66);
+//		component.setItemname("Cheese");
+//		component.setCalories(1000000);
+//
+//		return component;
+//	}
 
+	
 }
--- src/main/webapp/WEB-INF/index.jsp
+++ src/main/webapp/WEB-INF/index.jsp
@@ -1,74 +1,67 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
-  <head>
-    <title></title>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <script language="javascript">
-    var xmlhttp;
-    function init() {
-       // put more code here in case you are concerned about browsers that do not provide XMLHttpRequest object directly
-       xmlhttp = new XMLHttpRequest();
-    }
-    function getdetails() {
- //       var empno = document.getElementById("empno");
- 		var componentid = document.getElementById("componentid");
-//        var url = "http://localhost:8084/webservicedemo/resources/employee/" + empno.value;
-		var url = "http://localhost:8080/mealPlanner/data/components/" + componentId.value;
-        xmlhttp.open('GET',url,true);
-        xmlhttp.send(null);
-        xmlhttp.onreadystatechange = function() {
+<head>
+<title></title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<script language="javascript">
+	var xmlhttp;
+	function init() {
+		// put more code here in case you are concerned about browsers that do not provide XMLHttpRequest object directly
+		xmlhttp = new XMLHttpRequest();
+	}
+	function getdetails() {
+	
+		var componentid = document.getElementById("componentid");
+		
+		var url = "http://localhost:8080/mealPlanner/data/components/";
 
-//               var empname =  document.getElementById("empname");
- 			  var componentname =  document.getElementById("componentname");
-//               var age =  document.getElementById("age");
-				var componentcalories =  document.getElementById("componentcalories");
-               if (xmlhttp.readyState == 4) {
-                  if ( xmlhttp.status == 200) {
-                       var det = eval( "(" +  xmlhttp.responseText + ")");
-//                       if (det.age > 0 ) {
-					if (det.componentcalories > 0 ) {
- //                         empname.value = det.name;
- 					componentname.value = det.componentname;
-//                          age.value = det.age;
- 					componentcalories.value = det.componentcalories;
-                       }
-                       else {
-                           empname.value = "";
-  //                         age.value ="";
-   							componentcalories.value ="";
-  //                         alert("Invalid Employee ID");
-  							alert("Invalid component ID");
-                       }
-                 }
-                 else
-                       alert("Error ->" + xmlhttp.responseText);
-              }
-        };
-    }
-  </script>
-  </head>
-  <body  onload="init()">
-   <h1>Call Employee Service </h1>
-   <table>
-   <tr>
- <!--       <td>Enter Employee ID :  </td> -->
-	 <td>Enter Component ID :  </td>
-  <!--      <td><input type="text" id="empno" size="10"/>  <input type="button" value="Get Details" onclick="getdetails()"/> -->
-       <td><input type="text" id="componentid" size="10"/>  <input type="button" value="Get Details" onclick="getdetails()"/>
-   </tr>
-   <tr>
- <!--       <td>Enter Name :  </td> -->
-       <td>Enter Component Name :  </td>
-       <!-- <td><input type="text" readonly="true" id="empname" size="20"/> </td>  --> 
-       <td><input type="text" readonly="true" id="componentname" size="20"/> </td> 
-   </tr>
+		xmlhttp.open('GET', url, true);
+		xmlhttp.send(null);
+		xmlhttp.onreadystatechange = function() {
 
-   <tr>
-<!--        <td>Employee Age : </td> -->
-       <td>component calories : </td>
-<!--        <td><input type="text" readonly="true" id="age" size="10"/> </td> -->
-       <td><input type="text" readonly="true" id="componentcalories" size="10"/> </td>
-   </tr>
-   </table>
-  </body>
+			var componentname = document.getElementById("componentname");
+			var componentcalories = document.getElementById("componentcalories");
+			if (xmlhttp.readyState == 4) {
+
+				if (xmlhttp.status == 200) {
+					var det = eval("(" + xmlhttp.responseText + ")");
+					var i=0;
+					var tot = 0;
+					
+					for (tot=det.length; i < tot; i++) {
+							if (det[i].itemid == componentid.value) {
+								componentname.value = det[i].itemname;
+								componentcalories.value = det[i].calories;
+								break;
+							}
+					}  
+					
+				} else
+					alert("Error ->" + xmlhttp.responseText); 
+			}
+		};
+	}
+	
+</script>
+</head>
+<body onload="init()">
+	<h1>Component Lookup</h1>
+	<table>
+		<tr>
+			<td>Enter Component ID :</td>
+			<td><input type="text" id="componentid" size="10" /> 
+				<input type="button" value="Get Details" onclick="getdetails()" />
+		</tr>
+		<tr>
+			<td>Component Name :</td>
+			<td><input type="text" readonly="true" id="componentname" size="20" /></td>
+		</tr>
+
+		<tr>
+			<td>Component calories :</td>
+			<td><input type="text" readonly="true" id="componentcalories"
+				size="10" /></td>
+		</tr>
+	</table>
+</body>
 </html>
